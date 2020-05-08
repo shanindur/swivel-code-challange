47bc5387fdc23c43e9eb167e2dcc04b966265d54
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-08 16:56:14 +0530

    Added Colours and Font Sizes

---
+++ NewsApp/app/util/theme/colors.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+export default {
+	primaryColor: '#7b1fa2',
+	primaryLightColor: '#ae52d4',
+	primaryDarkColor: '#4a0072',
+	primaryTextColor: '#ffffff',
+
+	primaryFont: '#9ea3ac',
+	primaryIcon: '#6c6c74',
+	shadowColor: '#B9B9B9',
+
+	white: '#FFFFFF',
+	back: '#000000',
+
+	danger: '#FF5630',
+	warning: '#FFAB00',
+	success: '#36B37E',
+	info: '#00B8D9',
+};
---
+++ NewsApp/app/util/theme/fonts.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export default {
+	mini: 10,
+	extraSmall: 12,
+	small: 14,
+	medium: 16,
+	large: 18,
+	extraLarge: 20,
+};
