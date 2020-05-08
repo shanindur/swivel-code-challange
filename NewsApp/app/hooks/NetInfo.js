464f3d252632a4f351e4f9a030b61abac4d332e3
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-08 17:25:35 +0530

    Added NetInfo

---
+++ NewsApp/app/hooks/NetInfo.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Network connectivity hook
+ *
+ * @author       Shanindu Rajapaksha
+ *
+ * @version      0.1.0 2020-May-08
+ */
+import { useState, useEffect } from 'react';
+import NetInfo from '@react-native-community/netinfo';
+
+
+export const useNetInfo = () => {
+	const [ netInfo, setNetInfo ] = useState({isConnected: true});
+
+
+	useEffect(() => {
+		// Subscribe
+		const unsubscribe = NetInfo.addEventListener(state => {
+			setNetInfo(state);
+		});
+
+		// Event cleanup function
+		return () => {
+			// Unsubscribe
+			unsubscribe();
+		};
+	}, []);
+
+
+	return netInfo; // returns current network connection status
+};
