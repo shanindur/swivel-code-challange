e2019638c9191536e652131b8685ef593ac6becf
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-10 02:14:01 +0530

    Created screens  -> index.js

---
+++ NewsApp/app/screens/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/**
+ *Global News
+ *
+ *screens -> index
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.1.0 2020-May-09
+ */
+export {default as SplashScreen} from './splash';
+export {default as NoInternet} from './noInternet';
+export {default as Headline} from './headline';
+export {default as Profile} from './profile';
+export {default as News} from './news';
