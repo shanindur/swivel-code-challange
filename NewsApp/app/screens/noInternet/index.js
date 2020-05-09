b973594624bb6af515fde2ea7bd7b7f3104211fd
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-10 02:12:48 +0530

    Created No Internet Page

---
+++ NewsApp/app/screens/noInternet/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+/**
+ *Global News
+ *
+ * No Internet Screen
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.1.0 2020-May-09
+ */
+import React from 'react';
+import { SafeAreaView, ImageBackground, View, Text } from 'react-native';
+import styles from './style';
+
+const NoInternet = () => {
+	return (
+		<SafeAreaView>
+			<ImageBackground style={styles.background} source={require('../../assets/images/splash.jpg')}>
+				<View style={styles.container}>
+					<Text style={styles.titleText}>Connection Lost!</Text>
+					<Text style={styles.bodyText}>Please restore your Internet connection</Text>
+				</View>
+			</ImageBackground>
+		</SafeAreaView>
+	);
+};
+
+export default NoInternet;
---
+++ NewsApp/app/screens/noInternet/style.js
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+/**
+ *Global News
+ *
+ * No Internet Screen -> Style
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.1.0 2020-May-09
+ */
+import { StyleSheet } from 'react-native';
+import { Colors, Fonts } from '../../util';
+
+const styles = StyleSheet.create({
+	background: {
+		width: '100%',
+		height: '100%',
+
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+	},
+
+	container: {
+		width: '100%',
+		height: '40%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: Colors.lightGrey,
+	},
+
+	titleText: {
+		fontSize: 5 + Fonts.extraLarge,
+		color: Colors.danger,
+		fontWeight: 'bold',
+	},
+
+	bodyText: {
+		fontSize: Fonts.medium,
+		color: Colors.danger,
+	}
+});
+
+export default styles;
