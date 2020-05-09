e916b312b7babd53735522ec816ccafd38c34f1b
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-10 01:51:30 +0530

    Added Color and Font to util->index.js

---
+++ NewsApp/app/util/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Global News
+ *
+ * Util->index
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.0.1 2020-May-10
+ */
+export {default as Colors} from './theme/colors';
+export {default as Fonts} from './theme/fonts';
+
