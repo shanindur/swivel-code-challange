558a809028a168c371337249d3854241f2896f8b
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-10 18:08:27 +0530

    Created component index

---
+++ NewsApp/app/components/index.js
@@ -0,0 +1,11 @@
+/**
+ *Global News
+ *
+ *Components -> index
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.1.0 2020-May-09
+ */
+
+export {default as SquareButton} from './squareButton';
+export {default as Avatar} from './avatar';
