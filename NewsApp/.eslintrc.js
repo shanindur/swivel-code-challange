0f0e0623059af8859976759b8c121a814de15dcc
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-10 09:02:57 +0530

    Updated eslint

--- NewsApp/.eslintrc.js
+++ NewsApp/.eslintrc.js
@@ -1,4 +1,23 @@
+/**
+ * Ref:
+ *   - https://eslint.org/docs/user-guide/configuring
+ */
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	root: true,
+	extends: '@react-native-community',
+
+	// Enable additional rules
+	rules: {
+		// Use TAB for indentation
+		indent: ['error', 'tab', {
+//			ignoreComments: true,
+			SwitchCase: 1
+		}],
+
+		'comma-dangle': ["error", "never"],
+
+		// Use yoda style
+		yoda: ['error', 'always'],
+		'prettier/prettier': 0,
+	}
 };
