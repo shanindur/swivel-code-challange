586b2f315daef8b330141bb47e0f25c813f6e6ab
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-17 23:29:56 +0530

    Created Article component

---
+++ NewsApp/app/components/article/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, TouchableOpacity, Image } from 'react-native';
+import propTypes from 'prop-types';
+import styles from './style';
+
+const Article = ({ item, navigation}) => {
+	return (
+		<TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate('detail', { newsItem: item })}>
+			<Image
+				style={styles.itemImage}
+				source={{
+					uri: item.urlToImage
+				}}
+			/>
+			<Text style={styles.itemTitle}>{item.title}</Text>
+		</TouchableOpacity>
+	);
+};
+
+Article.propTypes = {
+	item: propTypes.object
+};
+
+export default Article;
---
+++ NewsApp/app/components/article/style.js
@@ -0,0 +1,33 @@
+/**
+ * Global News
+ *
+ * Article -> style
+ *
+ * @author       Shanindu Rajapaksha
+ * @version      0.0.1 2020-May-17
+ */
+import {StyleSheet} from 'react-native';
+import {Colors, Fonts} from '../../util';
+
+const styles = StyleSheet.create({
+	itemContainer: {
+		alignItems: 'center',
+		backgroundColor: Colors.lightGrey,
+		padding: 5,
+		marginVertical: 3,
+		marginHorizontal: 10
+	},
+	itemImage:{
+		width:'100%',
+		height:180
+	},
+	itemTitle: {
+		fontSize: Fonts.large,
+		padding: 10,
+		fontWeight: 'bold'
+	}
+});
+
+export default styles;
+
+
