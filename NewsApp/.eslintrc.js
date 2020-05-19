0ce44725991d95c63dee1fc0ed544692e4b63331
Shanindu Rajapaksha <shanindu.r@omobio.net>
2020-05-19 20:08:07 +0530

    Updated eslintrc.js

--- NewsApp/.eslintrc.js
+++ NewsApp/.eslintrc.js
@@ -8,16 +8,29 @@ module.exports = {
 
 	// Enable additional rules
 	rules: {
+		// Turn off Prettier
+		'prettier/prettier': 0,
+
 		// Use TAB for indentation
 		indent: ['error', 'tab', {
 //			ignoreComments: true,
 			SwitchCase: 1
 		}],
 
-		'comma-dangle': ["error", "never"],
-
 		// Use yoda style
 		yoda: ['error', 'always'],
-		'prettier/prettier': 0,
+		
+		'comma-dangle': ['error', 'never'],
+		'comma-spacing': [2, {'before': false, 'after': true}],
+		'key-spacing': ['error', {'beforeColon': false}],
+		'no-lonely-if': 'error',
+		'default-param-last': ['error'],
+		'no-var': 'error',
+		'prefer-const': 'error',
+		'no-duplicate-imports': ['error', {'includeExports': true}],
+		'rest-spread-spacing': ['error', 'never'],
+		'arrow-spacing': 'error',
+		'arrow-body-style': ['error', 'as-needed'],
+		'template-curly-spacing': ['error', 'never']
 	}
 };
